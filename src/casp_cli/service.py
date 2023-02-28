acb53b9be89a93c2c5d0b2fbad5bc41421bf64e6
Fedor Grab <fvgrab@gmail.com>
2023-02-28 09:19:38 -0800

    Include docstrings

--- src/casp_cli/service.py
+++ src/casp_cli/service.py
@@ -23,16 +23,36 @@ class _BaseService:
     BACKEND_URL: str
 
     def __init__(self, api_token: str, *args, **kwargs):
+        """
+        Base clas for communicating with a Cellarium Cloud API service
+        It leverages async request library `aiohttp` to asynchrounously execute HTTP requests.
+
+        :param api_token: A token that could be authenticated by Cellarium Cloud Backend API service
+        """
         self.api_token = api_token
         super().__init__(*args, **kwargs)
 
     @classmethod
     def _get_endpoint_url(cls, endpoint: str) -> str:
+        """
+        Configure a specific method endpoint from backend url and endpoint
+
+        :param endpoint: Endpoint string without a leading slash
+        :return: Full url with backend domains/subdomains and endpoint joint
+        """
         return f"{cls.BACKEND_URL}/{endpoint}"
 
     async def post(
         self, endpoint: str, file, data: t.Optional[t.Dict] = None, headers: t.Optional[t.Dict] = None
     ) -> t.Dict:
+        """
+        Make an async multiform POST request to backend service
+
+        :param endpoint: Endpoint string without a leading slash
+        :param file: Byte file to attach to request
+        :param data: Dictionary to include to HTTP POST request body
+        :param headers: Dictionary to include to HTTP POST request Headers
+        """
         url = self._get_endpoint_url(endpoint=endpoint)
         _data = {}
         _headers = {"Authorization": f"Bearer {self.api_token}"}
@@ -53,14 +73,18 @@ class _BaseService:
                 if resp.status == 401:
                     raise exceptions.HTTPError401
                 elif resp.status == 403:
-                    raise exception.HTTPError403
+                    raise exceptions.HTTPError403
                 elif resp.status == 500:
                     raise exceptions.HTTPError500
 
                 return await resp.json()
 
 
-class CASPClientService(_BaseService):
+class CASClientService(_BaseService):
+    """
+    Service that is designed to call Cellarium Cloud Backend API service
+    """
+
     BACKEND_URL = "https://cas-api-test-2-vi7nxpvk7a-uc.a.run.app"
 
     def _get_number_of_chunks(self, adata, chunk_size):
@@ -81,6 +105,18 @@ class CASPClientService(_BaseService):
         chunk_end_i: int,
         start_time: float,
     ) -> None:
+        """
+        A wrapper around POST request that handles HTTP 500 and HTTP 401 status responses
+        In case of HTTP 500 response resubmit task 2 times.
+        In case of HTTP 501 print a message
+
+        :param adata_bytes: Dumped bytes of anndata chunk
+        :param results: Results list that needs to be used to inplace the response from the server
+        :param chunk_index: Consequent number of the chunk (e.g Chunk 1, Chunk 2)
+        :param chunk_start_i: Index pointing to the main adata file start position of the current chunk
+        :param chunk_end_i: Index pointing to the main adata file end position of the current chunk
+        :param start_time: Start time of the execution before any task was submitted
+        """
         number_of_cells = chunk_end_i - chunk_start_i
         data = {"number_of_cells": str(number_of_cells)}
         for _ in range(3):
@@ -101,6 +137,14 @@ class CASPClientService(_BaseService):
                 break
 
     async def _annotate_anndata_task(self, adata, results, chunk_size, start_time) -> None:
+        """
+        Submit chunks asynchronously as asyncio tasks
+
+        :param adata: AnnData file to process
+        :param results: Results list that is used to inplace the responses from the server
+        :param chunk_sie: Chunk size to split on
+        :param start_time: Start time of the execution before this task was submitted
+        """
         i, j = 0, chunk_size
         tasks = []
         number_of_chunks = self._get_number_of_chunks(adata, chunk_size=chunk_size)
@@ -133,6 +177,17 @@ class CASPClientService(_BaseService):
         await asyncio.wait(tasks)
 
     def annotate_anndata(self, adata: "anndata.AnnData", chunk_size=2000) -> t.List:
+        """
+        Send an anndata object to Cellarium Cloud backend to get annotations. Split the input
+        adata to smaller chunks and send them all asynchronously to the backend API service.
+        All of the chunks have equal size apart of the last one, which is usually smaller than
+        the rest of the chunks. Backend API processes all of these chunks in parallel and
+        returns them as soon as they are ready.
+
+        :param adata: AnnData instance to annotate
+        :param chunk_size: Size of chunks to split on
+        :return: A list of dictionaries with annotations for each of the cells from input adata
+        """
         start = time.time()
         self._print("CAS v1.0 (Model ID: PCA_002)")
         self._print(f"Total number of input cells: {len(adata)}")
